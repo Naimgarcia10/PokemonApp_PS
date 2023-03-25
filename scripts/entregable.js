a12d39ca7c27b074f4953f083f0e9865c7839e6a
Victor Zuluaga <vj_626@hotmail.com>
2023-03-25 08:50:43 +0000

    puesto los json de parte del servidor y arreglada la ruta relativa al archivo por parte del servidor

--- scripts/entregable.js
+++ scripts/entregable.js
@@ -1,44 +1,69 @@
+//asocio las acciones a los elementos 
 const flecha_derecha = document.getElementById("flecha_derecha");
 const flecha_izquierda = document.getElementById("flecha_izquierda");
 flecha_derecha.addEventListener("click", http_tutorial);
 flecha_izquierda.addEventListener("click", http_tutorial);
-var contador_paginas = 0;
+
+//controlador de pagina seleccionada y rango de tutorial
+let ultima_pagina_ok;
+let contador_paginas = 0;
 const tutorial_size = 2;
+
+//funcion que se acciona con los eventos de las flechas del tutorial
 function http_tutorial(event) {
-    fetch('../json/tutorial.json')
-        .then(response => response.json())
-        .then(data => {
-            try {
-                var elementId = event.target.id;
-                if (elementId == "flecha_derecha") {
-                    contador_paginas++;
-                    console.log("pasar a derecha");
-                    var imagen = document.getElementById("imagen-tutorial");
-                    var texto = document.getElementById("texto-tutorial");
-                    imagen.src = data[contador_paginas].ruta_imagen;
-                    texto.textContent = data[contador_paginas].texto;
-                }
 
-                if (elementId == "flecha_izquierda") {
-                    console.log("pasar a izquierda");
-                    contador_paginas--;
-                    var imagen = document.getElementById("imagen-tutorial");
-                    var texto = document.getElementById("texto-tutorial");
-                    imagen.src = data[contador_paginas].ruta_imagen;
-                    texto.textContent = data[contador_paginas].texto;
-                }
-            } catch (error) {
-                if (elementId == "flecha_derecha"){
-                    contador_paginas = 0;
-                    imagen.src = data[contador_paginas].ruta_imagen;
-                    texto.textContent = data[contador_paginas].texto;
-                }
-                if (elementId == "flecha_izquierda"){
-                    contador_paginas = tutorial_size;
-                    imagen.src = data[contador_paginas].ruta_imagen;
-                    texto.textContent = data[contador_paginas].texto;
-                }
+    //registro del id del accionador
+    let elementId = event.target.id;
+
+    //si va para la derecha se suma el contador_paginas
+    if (elementId == "flecha_derecha") {
+
+        contador_paginas++;
+
+    } else if (elementId == "flecha_izquierda") {   //si va para la izquierda se resta el contador_paginas
+
+        contador_paginas--;
+
+    } else {  //si no fue ninguna de las condiciones ha habiado un error de accionador
+
+        return "error: evento no registrado";
+
+    }
+
+    if (contador_paginas > tutorial_size) {
+
+        contador_paginas = 0;   //en caso de pedir la siguiente pagina y haber superado el tamaño, vuelves a la pagina 0
+
+    } else if (contador_paginas < 0) {
+
+        contador_paginas = tutorial_size;   //en caso de pedir la pagina menos 1, vuelves a la ultima pagina
+
+    }
+
+    //selecciona los elementos a cambiar
+    let imagen = document.getElementById("imagen-tutorial");
+    let texto = document.getElementById("texto-tutorial");
+
+    //realizo la petición http, si el tutorial tiene 3 paginas, ira de 0 a 2
+    fetch('http://localhost:8080/tutorial/' + contador_paginas)
+        .then(response => {
+            if(response.ok){
+                ultima_pagina_ok = contador_paginas;    //almaceno el valor las ultimas respuestas que fueron exitosa
+                return response.json();
             }
-
         })
+        .then(data => {
+
+            //cargo los nuevos contenidos
+            imagen.src = data.ruta_imagen;
+            texto.textContent = data.texto;
+
+        }).catch(error => {
+
+            //en caso de error con el servidor, no aumento el contador
+            console.log('Error en la petición:', error);
+            contador_paginas = ultima_pagina_ok;
+
+        });
+
 }
