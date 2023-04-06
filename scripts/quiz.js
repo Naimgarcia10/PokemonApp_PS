c0ed8239108c004173dc4717a319fb29e975ada6
PedroRS9 <pedroglv98@gmail.com>
2023-04-06 16:02:49 +0100

    Quiz fijo

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,6 +1,7 @@
 // VARIABLES DEL PROGRAMA
 var preguntas = [];
 var respuestas_usuario;
+var opciones_elementos = []
 var indice;
 const imagePath = "../images/";
 const questionPath = "/questions/";
@@ -11,6 +12,11 @@ fetch('../json/questions.json')
 	.then(response => response.json())
 	.then(data => {
 		preguntas = data;  
+        for(let i=0;i<preguntas.length;i++){
+            preguntas[i].answers = preguntas[i].incorrectAnswers;
+            preguntas[i].answers.push(preguntas[i].correctAnswer);
+            randomizar_array(preguntas[i].answers);
+        }
 	})
 	.catch(error => console.log(error));
 
@@ -19,11 +25,12 @@ document.addEventListener("DOMContentLoaded", function(){
     start_button.addEventListener("click", empezar_quiz);
 });
 
-function empezar_quiz(){
+function empezar_quiz(){        
     respuestas_usuario = [];
     indice = 0;
+    document.addEventListener("keydown", manejar_pulsaciones);
     ocultar_elementos();   
-    mostrar_elementos(); 
+    mostrar_elementos();
     cargar_pregunta();
 }
 
@@ -33,7 +40,8 @@ function finalizar_quiz(){
         let opcion_correcta = preguntas[i].correctAnswer;
         let opcion_usuario = respuestas_usuario[i];
         puntos += (opcion_correcta === opcion_usuario);           
-    }        
+    }
+    document.removeEventListener("keydown", manejar_pulsaciones);
     mostrar_nota(puntos);
 }
 
@@ -44,6 +52,19 @@ function ocultar_elementos(){
     }   
     const start_button = document.querySelector(".cuadrado_quiz .start_button");
     start_button.style.display = "none";
+}
+
+function manejar_pulsaciones(event){
+    let teclaPulsada = event.key;
+    if(teclaPulsada === 'ArrowLeft') go_left();
+    else if(teclaPulsada === 'ArrowRight') go_right();
+    else if(teclaPulsada.length === 1){        
+        let numero = parseInt(teclaPulsada);
+        let es_numero = (numero != NaN);
+        if(es_numero && numero <= opciones_elementos.length){
+            marcar_opcion(opciones_elementos[numero-1])
+        }
+    }
 }
 
 function mostrar_elementos(){
@@ -68,11 +89,14 @@ function cargar_pregunta(){
         imagen.src = "";
     }
     borrar_opciones();
-    let lista_opciones = preguntas[indice]["incorrectAnswers"].slice();
-    lista_opciones.push(preguntas[indice].correctAnswer);    
+    let lista_opciones = preguntas[indice]["answers"].slice();           
     for(let i=0;i<lista_opciones.length;i++){
         añadir_opcion(lista_opciones[i], i);
     }
+}
+
+function randomizar_array(array){
+    array.sort(() => Math.random() - 0.5);
 }
 
 function añadir_opcion(name, number){
@@ -91,26 +115,33 @@ function añadir_opcion(name, number){
     let options_list = document.querySelector("#opciones");
     options_list = document.querySelector("#opciones");          
     options_list.appendChild(opt_element);      
+    opciones_elementos.push(opt_element);
 
     /* Añadimos un event listener para cambiar el color cada vez que se pulsa uno */
-    opt_element.addEventListener("click", function(){    
-        if(opt_element.classList.contains("marcada")){            
-            opt_element.classList.remove("marcada"); 
-            respuestas_usuario[indice] = "";           
-        } else{
-            // comprobamos que solo haya una opción marcada (por el momento las preguntas son de una sola opción)
-            for(let i=0;i<options_list.children.length;i++){
-                options_list.children[i].classList.remove("marcada");
-            }
-            opt_element.classList.add("marcada");
-            respuestas_usuario[indice] = opt_element.textContent;
-        }        
-    })
+    opt_element.addEventListener("click", function(){
+        marcar_opcion(opt_element);
+    });
+}
+
+function marcar_opcion(opt_element){
+    if(opt_element.classList.contains("marcada")){            
+        opt_element.classList.remove("marcada"); 
+        respuestas_usuario[indice] = "";           
+    } else{        
+        // comprobamos que solo haya una opción marcada (por el momento las preguntas son de una sola opción)
+        let options_list = document.querySelector("#opciones");
+        for(let i=0;i<options_list.children.length;i++){
+            options_list.children[i].classList.remove("marcada");
+        }
+        opt_element.classList.add("marcada");
+        respuestas_usuario[indice] = opt_element.textContent;
+    }
 }
 
 function borrar_opciones(){
     let options_list = document.querySelector("#opciones");    
     options_list.innerHTML = "";
+    opciones_elementos = [];
 }
 
 function go_left(){
@@ -133,6 +164,7 @@ function mostrar_nota(puntos){
     borrar_opciones();
     let enunciado = document.querySelector(".enunciado");
     enunciado.innerHTML = "&nbsp;";
+    enunciado.classList.add("hide");
 
     let left_arrow = document.querySelector("#left_button");
     left_arrow.style.display = "none";
@@ -146,7 +178,7 @@ function mostrar_nota(puntos){
     let subdescription = cuadrado_quiz.querySelector("#quiz_subdescription");
     subdescription.textContent = puntos + "/" + preguntas.length;    
     let description2 = cuadrado_quiz.querySelector("#quiz_description2");
-    let numero_preguntas = preguntas.length;
+    let numero_preguntas = preguntas.length;    
 
     let aprobado = (puntos >= (numero_preguntas / 2));
     if(aprobado){
@@ -162,6 +194,7 @@ function mostrar_nota(puntos){
     let reset_button = document.querySelector("#reset_button");
     reset_button.style.display = "inline-block";
     reset_button.addEventListener("click", function(){
+        enunciado.classList.remove("hide");
         cuadrado_quiz.classList.remove("aprobado");
         cuadrado_quiz.classList.remove("suspendido");
         reset_button.style.display = "none";
