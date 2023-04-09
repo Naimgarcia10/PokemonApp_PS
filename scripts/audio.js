6bd4a22f6941b24eb3606b9836805416df39b949
PedroRS9 <pedroglv98@gmail.com>
2023-04-09 21:31:04 +0100

    Música en quiz y tutorial

--- scripts/audio.js
+++ scripts/audio.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded",()=>{
 
     let imagen_audio = document.getElementById("imagen_boton_audio");
-    imagen_audio.src = "../icons/boton_play.png";
+    imagen_audio.src = "../icons/pikachu_play.png";
 
     imagen_audio.addEventListener("click",()=>{
         let audio = document.getElementById("audio");
@@ -9,10 +9,10 @@ document.addEventListener("DOMContentLoaded",()=>{
         //audio.loop;
         if(audio.paused){
             audio.play();
-            imagen_audio.src = "../icons/boton_pause.png";
+            imagen_audio.src = "../icons/pikachu_pause.png";
         }else{
             audio.pause();
-            imagen_audio.src = "../icons/boton_play.png";
+            imagen_audio.src = "../icons/pikachu_play.png";
         }
     
     });
