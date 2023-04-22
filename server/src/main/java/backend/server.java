45ba22abfef4d79a86f03f945e3962fab5890bb3
Victor Zuluaga <vj_626@hotmail.com>
2023-04-22 10:49:51 +0100

    static files

--- server/src/main/java/backend/server.java
+++ server/src/main/java/backend/server.java
@@ -1,18 +1,23 @@
 package backend;
 
+import org.eclipse.jetty.http.MetaData.Response;
+
 import spark.Spark;
 
-public class server 
-{
-    public static void main( String[] args )
-    {   
+public class server {
+    public static void main(String[] args) {
         
+        Spark.staticFiles.location("");
         Spark.port(8080);
-        Spark.get("/tutorial/:index", (rq,rs)->{
+        /*Spark.get("/tutorial/:index", (rq, rs) -> {
             String param = rq.params(":index");
             int index = Integer.parseInt(param);
             return jsonManager.get_tutorial(index);
         });
-
+        String sDirectorioTrabajo = System.getProperty("user.dir");
+        System.out.println("El directorio de trabajo es " + sDirectorioTrabajo);*/
+        Spark.get("/", (req, res) -> {
+            return "";
+        });  
     }
 }
