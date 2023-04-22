05845342025d93ec32acabeba0dabb7deaeb6543
Victor Zuluaga <vj_626@hotmail.com>
2023-04-22 15:54:46 +0100

    conseguida conexion con base de datos remota

--- server/src/main/java/backend/server.java
+++ server/src/main/java/backend/server.java
@@ -1,12 +1,11 @@
 package backend;
 
-//import org.eclipse.jetty.http.MetaData.Response;
-
 import spark.Spark;
+import java.sql.*;
 
 public class server {
     public static void main(String[] args) {
-        
+
         Spark.staticFiles.location("");
         Spark.port(8080);
         Spark.get("/", (req, res) -> {
@@ -14,10 +13,35 @@ public class server {
             return "";
         });
 
-        Spark.get("/tutorial/:index", (rq,rs)->{
+        Spark.get("/tutorial/:index", (rq, rs) -> {
             String param = rq.params(":index");
             int index = Integer.parseInt(param);
             return jsonManager.get_tutorial(index);
         });
+
+        /*Base de datos*/
+        String url = "jdbc:mysql://localhost:3306/sakila";
+        String user = "root";
+        String password = "12345";
+
+        String url_remote = "jdbc:mysql://beugk46erqljqvqdzm9s-mysql.services.clever-cloud.com:3306/beugk46erqljqvqdzm9s";
+        String user_remote = "u2j5iqnwhemyff19";
+        String password_remote = "JEiD6HzCOlfVMHBYmH0T";
+        
+
+        try{
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(url_remote, user_remote, password_remote);
+
+            if(conn!=null){
+                System.out.println("CONEXION EXITOSA");
+            }
+
+        }catch(Exception e){
+            System.out.println("-------------------------" + e);
+        }
+        
+
     }
 }
