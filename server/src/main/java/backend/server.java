4c31b74bc639f34cefd1654497693767bf45ed46
Victor Zuluaga <vj_626@hotmail.com>
2023-04-22 11:25:56 +0100

    todo en la parte servidor menos tutorial

--- server/src/main/java/backend/server.java
+++ server/src/main/java/backend/server.java
@@ -9,15 +9,9 @@ public class server {
         
         Spark.staticFiles.location("");
         Spark.port(8080);
-        /*Spark.get("/tutorial/:index", (rq, rs) -> {
-            String param = rq.params(":index");
-            int index = Integer.parseInt(param);
-            return jsonManager.get_tutorial(index);
-        });
-        String sDirectorioTrabajo = System.getProperty("user.dir");
-        System.out.println("El directorio de trabajo es " + sDirectorioTrabajo);*/
         Spark.get("/", (req, res) -> {
+            res.redirect("/html/home.html");
             return "";
-        });  
+        });
     }
 }
---
+++ server/target/classes/scripts/components/footer.js
@@ -0,0 +1,2 @@
+import { loadOver } from "./function.js"
+loadOver("footer.html", document.querySelector('script[src="../scripts/components/footer.js"]'))
