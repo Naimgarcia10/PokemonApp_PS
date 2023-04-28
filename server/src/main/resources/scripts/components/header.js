aae537d0aca8f75afd7e076aafd05dc5eff7cc5a
PedroRS9 <pedroglv98@gmail.com>
2023-04-28 10:58:35 +0100

    Selector de idiomas

--- server/src/main/resources/scripts/components/header.js
+++ server/src/main/resources/scripts/components/header.js
@@ -1,2 +1,16 @@
 import { loadOver } from "./function.js"
-loadOver("header.html", document.querySelector('script[src="../scripts/components/header.js"]'))
+
+export const cargarSelectorIdiomas = () => {
+    const languagePickerSelect = document.getElementById('language-picker-select');
+    if(localStorage.getItem("userLanguage") === "es") languagePickerSelect.value = "spanish";
+    languagePickerSelect.addEventListener('change', () => {
+        const selectedLang = languagePickerSelect.selectedOptions[0].getAttribute("lang");
+        localStorage.setItem("userLanguage", selectedLang);
+        console.log("El idioma seleccionado por el usuario es ", selectedLang);
+    });
+}
+
+loadOver("header.html", document.querySelector('script[src="../scripts/components/header.js"]'));
+document.addEventListener("HeaderCargado", function(){
+    cargarSelectorIdiomas();
+})
--- server/src/main/resources/scripts/components/header_expertos.js
+++ server/src/main/resources/scripts/components/header_expertos.js
@@ -1,6 +1,8 @@
 import { loadOver } from "./function.js"
+import { cargarSelectorIdiomas } from "./header.js";
 loadOver("header.html", document.querySelector('script[src="../scripts/components/header_expertos.js"]'))
 document.addEventListener("HeaderCargado", function(){
     // mostrar header expertos
     document.querySelector("header .navbar_expertos").style.display = "block";
+    cargarSelectorIdiomas();
 })
