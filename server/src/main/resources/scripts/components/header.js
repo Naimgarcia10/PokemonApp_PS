a50ae0ccf2518693add9b53dc9906882f3db24db
PedroRS9 <pedroglv98@gmail.com>
2023-05-02 16:02:34 +0100

    Banderas para seleccionar idioma

--- server/src/main/resources/scripts/components/header.js
+++ server/src/main/resources/scripts/components/header.js
@@ -1,13 +1,15 @@
 import { loadOver } from "./function.js"
 
 export const cargarSelectorIdiomas = () => {
-    const languagePickerSelect = document.getElementById('language-picker-select');
-    if(localStorage.getItem("userLanguage") === "es") languagePickerSelect.value = "spanish";
-    languagePickerSelect.addEventListener('change', () => {
-        const selectedLang = languagePickerSelect.selectedOptions[0].getAttribute("lang");
-        localStorage.setItem("userLanguage", selectedLang);
-        console.log("El idioma seleccionado por el usuario es ", selectedLang);
+    const spanishFlag = document.querySelector("#spanish");
+    const englishFlag = document.querySelector("#english");
+    spanishFlag.addEventListener("click", () => {
+        localStorage.setItem("userLanguage", "es");
         location.reload();
+    });
+    englishFlag.addEventListener("click", () => {
+        localStorage.setItem("userLanguage", "en");
+         location.reload();
     });
 }
 
