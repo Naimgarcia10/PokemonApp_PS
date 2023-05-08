e5f5b8f507ca4050441ec6134cdee3bfc5a7dba8
Victor Zuluaga <vj_626@hotmail.com>
2023-05-08 11:43:52 +0100

    añadido boton de guardar

--- server/src/main/resources/scripts/equipoPokemon.js
+++ server/src/main/resources/scripts/equipoPokemon.js
@@ -1,6 +1,7 @@
 const card = document.querySelector('.card');
 const overlay = document.querySelector('.overlay');
 const closeBtn = document.querySelector('.close-btn');
+const saveBtn = document.querySelector('.save-btn');
 
 card.addEventListener('click', function() {
   overlay.style.display = 'block';
@@ -9,3 +10,8 @@ card.addEventListener('click', function() {
 closeBtn.addEventListener('click', function() {
   overlay.style.display = 'none';
 });
+
+saveBtn.addEventListener('click', function() {
+  overlay.style.display = 'none';
+});
+
