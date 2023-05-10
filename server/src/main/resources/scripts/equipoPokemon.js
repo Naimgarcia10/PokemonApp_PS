40b1e8a40c9f044233243cf9afafcd83d6f2d2ba
Victor Zuluaga <vj_626@hotmail.com>
2023-05-10 14:00:42 +0100

    añadido buscador y funciona perfecto

--- server/src/main/resources/scripts/equipoPokemon.js
+++ server/src/main/resources/scripts/equipoPokemon.js
@@ -3,15 +3,55 @@ const overlay = document.querySelector('.overlay');
 const closeBtn = document.querySelector('.close-btn');
 const saveBtn = document.querySelector('.save-btn');
 
-card.addEventListener('click', function() {
-  overlay.style.display = 'block';
+card.addEventListener('click', function () {
+    overlay.style.display = 'block';
 });
 
-closeBtn.addEventListener('click', function() {
-  overlay.style.display = 'none';
+closeBtn.addEventListener('click', function () {
+    overlay.style.display = 'none';
 });
 
-saveBtn.addEventListener('click', function() {
-  saveBtn.classList.toggle('green-background');
+saveBtn.addEventListener('click', function () {
+    overlay.style.display = 'none';
 });
+
+
+const pokemonList = ["Bulbasaur", "Charmander", "Squirtle", "Pikachu", "Eevee", "Snorlax", "Mew", "Mewtwo"];
+const input = document.getElementById("input-text");
+const pokemonUl = document.getElementById("pokemon-list");
+
+input.addEventListener("input", filtradoPokemon);
+//input.addEventListener("click", filtradoPokemon);
+
+function filtradoPokemon() {
+    const searchQuery = this.value.toLowerCase();
+    const filteredPokemon = pokemonList.filter(pokemon => pokemon.toLowerCase().startsWith(searchQuery));
+    pokemonUl.innerHTML = ""; // Limpiar la lista antes de agregar elementos filtrados
+    filteredPokemon.forEach(pokemon => {
+        const li = document.createElement("li");
+        li.innerText = pokemon;
+        li.addEventListener("click", () => {
+            const valor = li.textContent;
+            document.getElementById('input-text').value = valor;
+            pokemonUl.innerHTML = "";
+        });
+        pokemonUl.appendChild(li);
+
+    });
+
+}
+
+const searchBox = document.getElementById('input-text');
+const searchButton = document.getElementById('search-button');
+searchButton.onclick = () => {
+    console.log("a buscar pokemon");
+    
+    //Borrar contenido del input
+    searchBox.value = "";
+    //Borrar contenido de la lista
+    pokemonUl.innerHTML = "";
+};
+
+
+
 
