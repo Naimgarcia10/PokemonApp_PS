a314e254c870e0f7937809feb77d278125d6e1d6
Sullivan Marin Garcia <sullivan.marin20@gmail.com>
2023-05-18 12:57:37 +0100

    Añadido canal de sugerencias

--- server/src/main/resources/scripts/emailHandler.js
+++ server/src/main/resources/scripts/emailHandler.js
@@ -4,12 +4,24 @@ document.getElementById('suggestion-form').addEventListener('submit', async func
     const email = document.getElementById('email').value;
     const suggestion = document.getElementById('suggestion').value;
     const loadingBar = document.getElementById('loading-bar');
+    const modal = document.getElementById('myModal');
+    const span = document.getElementsByClassName("close")[0];
 
     loadingBar.style.display = 'block'; // Mostrar la barra de carga
 
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+
     try {
         await sendEmail('pokemonpsapp@gmail.com', 'Suggestion', `Correo: ${email}\n\nSugerencia: ${suggestion}`);
-        alert('Correo enviado exitosamente');
+        modal.style.display = "block"; // Mostrar el modal
     } catch (error) {
         console.error('Error al enviar el correo:', error);
         alert('Error al enviar el correo');
